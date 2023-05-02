e59642acf500ab3eebfdaca4a34a79cc6232ce56
Lewin <lewinkoon+github@gmail.com>
2023-05-02 20:29:42 +0200

    comment unused helpers

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
-	"runtime/debug"
 )
 
-func (app *application) serverError(w http.ResponseWriter, err error) {
+/* func (app *application) serverError(w http.ResponseWriter, err error) {
 	trace := fmt.Sprintf("%s\n%s", err.Error(), debug.Stack())
 	app.errorLog.Output(2, trace)
 	http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusIMUsed)
-}
+} */
 
 func (app *application) clientError(w http.ResponseWriter, status int) {
 	http.Error(w, http.StatusText(status), status)
