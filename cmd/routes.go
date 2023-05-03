79530dac6dbe476f7feb52a47ec6d05bbb43aa3c
Lewin <lewinkoon+github@gmail.com>
2023-05-03 16:35:46 +0200

    replace http.ServeMux with chi

--- cmd/routes.go
+++ cmd/routes.go
@@ -1,11 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"github.com/go-chi/chi/v5"
+)
 
-func (app *application) routes() *http.ServeMux {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/level/", app.level)
+func (app *application) routes() *chi.Mux {
+	mux := chi.NewRouter()
+	mux.Get("/", app.home)
+	mux.Get("/level/", app.level)
 
 	return mux
 
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/antchfx/htmlquery v1.3.0 // indirect
 	github.com/antchfx/xmlquery v1.3.15 // indirect
 	github.com/antchfx/xpath v1.2.3 // indirect
+	github.com/go-chi/chi/v5 v5.0.8 // indirect
 	github.com/gobwas/glob v0.2.3 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
